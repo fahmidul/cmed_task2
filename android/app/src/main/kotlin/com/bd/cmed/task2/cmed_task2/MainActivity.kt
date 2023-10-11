8b5af20f9d79119d7b0d8f80df3a16be09f6068e
fahmidul <fahmidulp@gmail.com>
2023-10-11 22:00:16 +0600

    Initial project setup

---
+++ android/app/src/main/kotlin/com/bd/cmed/task2/cmed_task2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bd.cmed.task2.cmed_task2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await startInitialize();
+  // runApp();
+}
+
+Future<void> startInitialize() async {}
